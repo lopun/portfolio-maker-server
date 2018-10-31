81e7b78d256e92f44e038ac8db582cde5b4acd50
lopun <ujin43255252@gmail.com>
2018-10-31 21:27:48 +0900

    AllProject & ALlResumes Done

--- src/api/Project/AllProjects/AllProjects.resolvers.ts
+++ src/api/Project/AllProjects/AllProjects.resolvers.ts
@@ -1,12 +1,15 @@
 import { Resolvers } from "src/types/resolvers";
 import { AllProjectsResponse } from "src/types/graph";
 import Project from "src/entities/Project";
+import { getRepository } from "typeorm";
 
 const resolvers: Resolvers = {
   Query: {
     AllProjects: async (_, __, { req }): Promise<AllProjectsResponse> => {
       try {
-        const projects = await Project.find();
+        const projects = await getRepository(Project).find({
+          relations: ["author"]
+        });
         return {
           ok: true,
           error: null,
--- src/api/Resume/AllResumes/AllResumes.resolvers.ts
+++ src/api/Resume/AllResumes/AllResumes.resolvers.ts
@@ -1,12 +1,15 @@
 import { Resolvers } from "src/types/resolvers";
 import Resume from "src/entities/Resume";
 import { AllResumesResponse } from "src/types/graph";
+import { getRepository } from "typeorm";
 
 const resolvers: Resolvers = {
   Query: {
     AllResumes: async (_, __, ___): Promise<AllResumesResponse> => {
       try {
-        const resumes: Resume[] = await Resume.find();
+        const resumes: Resume[] = await getRepository(Resume).find({
+          relations: ["author"]
+        });
         return {
           ok: true,
           error: null,
